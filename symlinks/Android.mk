8c5f6fec13ea489c07d7925fb6cba8154ed38795
Alfred Neumayer <dev.beidl@gmail.com>
2021-08-23 00:09:10 +0200

    symlinks: Enable custom audio HAL
    Requires: https://github.com/Halium/hybris-patches/pull/25

--- symlinks/Android.mk
+++ symlinks/Android.mk
@@ -1,5 +1,10 @@
-# Symlink to Adreno GLESv2 blobs
 $(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib/)
 $(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib64/)
+$(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib/hw)
+$(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib64/hw)
 $(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib/ > /dev/null && ln -s /vendor/lib/egl/libGLESv2_adreno.so libGLESv2_adreno.so && popd > /dev/null)
 $(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib64/ > /dev/null && ln -s /vendor/lib64/egl/libGLESv2_adreno.so libGLESv2_adreno.so && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib/hw/ > /dev/null && ln -s audio.primary.sdm710.so audio.primary.bonito.so && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/ > /dev/null && ln -s audio.primary.sdm710.so audio.primary.bonito.so && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib/hw/ > /dev/null && ln -s audio.primary.sdm710.so audio.primary.sargo.so && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/ > /dev/null && ln -s audio.primary.sdm710.so audio.primary.sargo.so && popd > /dev/null)
