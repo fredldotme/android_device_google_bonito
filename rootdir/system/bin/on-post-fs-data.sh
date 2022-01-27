f7d003f1b9c8e4082f3878955ed23c28b318c706
Alfred Neumayer <dev.beidl@gmail.com>
2022-01-27 22:20:27 +0100

    on-post-fs-data: Fix voice call volume
    The fixed Qualcomm audio HAL was expecting a different property
    to be set to correctly determine availability of Fluence.
    Enable it + the two call-related fluence properties!

--- rootdir/system/bin/on-post-fs-data.sh
+++ rootdir/system/bin/on-post-fs-data.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 
-setprop persist.audio.fluence.voicecall false
-setprop persist.audio.fluence.speaker false
+setprop ro.qc.sdk.audio.fluencetype fluencepro
+setprop persist.audio.fluence.voicecall true
+setprop persist.audio.fluence.speaker true
 bootctl mark-boot-successful
