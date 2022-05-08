6f4f77bb27a5399a8804b579775de57b8155d550
Alfred Neumayer <dev.beidl@gmail.com>
2022-05-08 17:13:08 +0200

    on-post-fs-data: Tweak CPU scheduler boost settings
    - Enable sched_load_boost
    - Set hispeed_freq to the highest possible value
      to ramp up performance during heavy load

--- rootdir/system/bin/on-post-fs-data.sh
+++ rootdir/system/bin/on-post-fs-data.sh
@@ -1,3 +1,10 @@
 #!/system/bin/sh
 
+for i in $(seq 0 7);
+do
+    echo -6 > /sys/devices/system/cpu/cpu${i}/sched_load_boost
+    HIGHEST_FREQ=$(cat /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_available_frequencies | awk '{print $(NF)}')
+    echo $HIGHEST_FREQ > /sys/devices/system/cpu/cpu${i}/cpufreq/schedutil/hispeed_freq
+done
+
 bootctl mark-boot-successful
